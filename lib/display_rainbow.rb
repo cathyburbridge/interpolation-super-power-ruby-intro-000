c52ee45b4bd102a60ebecc96f5668b66782f627d
Cathy Burbridge <cathyburbridge@outlook.com>
2018-12-19 13:09:45 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,9 +4,3 @@ def display_rainbow(colours, x)
   puts "R: #{colours[0]}, O: #{colours[1]}, Y: #{colours[2]}, G: #{colours[3]"
 end
   
-
-  display_rainbow("The #{0} in the rainbow is #{colour[0]}", 7)
-  puts "The first colour in the rainbow is: #{colour[0]}"
-  puts "The second colour in the rainbow is: #{colour[1]}"
-  puts
-end
